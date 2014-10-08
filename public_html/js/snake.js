a10f6831685f63b9f55180ded89e7e8ec2e91296
nareg-agopian <nareg19@outlook.com>
2014-10-08 14:53:04 -0700

    initial project i made

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -5,7 +5,14 @@ var screenWidth;
 var screenHeight;
 
 function gameInitialize() {
+    var canvas = document.getElementById("game-screen");
+    context = canvas.getContext("2d");
     
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    
+    canvas.widht = screenWidht;
+    canvas.height = screenHeight;
 }
 
 function gameLoop() {
